1f3030870e4b24b0a468a2acd4c29f7e6ddbd3cf
rscheiwe <rscheiwe@gmail.com>
2018-06-29 15:41:36 -0400

    rails finish

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,6 +1,41 @@
 class SeasController < ApplicationController
   #define your controller actions here
 
+  #resources :seas, only: [:index, :show, :new, :edit, :delete]
+
+  def welcome
+
+  end
+
+  def index
+    @seas = Sea.all
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def new
+    @sea = Sea.new(params[:sea])
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def update
+    @sea = Sea.find(params[:id])
+    @sea.update(sea_params)
+    @sea.save
+    redirect_to sea_path(@sea)
+  end
+
+  def destroy
+    @sea = Sea.find(params[:id])
+
+    @sea.destroy
+    redirect_to seas_path
+  end
 
   private
   # In controller actions, use this private method to access sea params from forms.
--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :seas, only: [:index, :show, :new, :update, :edit, :destroy]
+  get "/", to: "seas#welcome"
+
 end
